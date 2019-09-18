076f344fda25b3f0bd78cb4f98691cd09be3f946
HongDong <1013307264@qq.com>
2019-09-18 18:36:14 +0800

    晚上了

--- src/com/atguigu/egit/test/com/atguigu/egit/test/Egit_test.java
+++ src/com/atguigu/egit/test/com/atguigu/egit/test/Egit_test.java
@@ -1,8 +1,9 @@
-package com.atguigu.egit.test;
+package com.atguigu.egit.test.com.atguigu.egit.test;
 
 public class Egit_test {
 	public static void main(String[] args) {
 		System.out.println("sssss");
 		System.out.println("xxxxxx");
+		System.out.println("德玛西亚");
 	}
 }
